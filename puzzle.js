a6ced5299fd9d3ec92ada0e8dc8cf60422f2f23a
Carlos <carlosmbollero@gmail.com>
2015-05-18 12:05:22 -0300

    Agregado codigo para partir y mezclar la imagen

---
+++ puzzle.js
@@ -0,0 +1,58 @@
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var cw = canvas.width;
+var ch = canvas.height;
+
+var rows = 3;
+var cols = 3;
+
+var img = new Image();
+
+function shuffle(a) {
+    for (var j, x, i = a.length; i; j = Math.floor(Math.random() * i), x = a[--i], a[i] = a[j], a[j] = x);
+    return a;
+};
+
+function start() {
+
+    var iw = canvas.width = img.width;
+    var ih = canvas.height = img.height;
+    var pieceWidth = iw / cols;
+    var pieceHeight = ih / rows;
+
+    var pieces = [
+        {col: 0, row: 0},
+        {col: 1, row: 0},
+        {col: 2, row: 0},
+        {col: 0, row: 1},
+        {col: 1, row: 1},
+        {col: 2, row: 1},
+        {col: 0, row: 2},
+        {col: 1, row: 2},
+        {col: 2, row: 2}];
+    shuffle(pieces);
+
+        var i=0;
+        for(var y = 0;y < rows; y++){
+        for(var x = 0;x < cols; x++){
+        var p = pieces[i++];
+  ctx.drawImage(
+    // from the original image
+    img,
+    // take the next x,y piece
+    x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight,
+    // draw it on canvas based on the shuffled pieces[] array
+    p.col * pieceWidth, p.row * pieceHeight, pieceWidth, pieceHeight
+  );
+}}
+
+
+}
+
+
+img.onload = start;
+img.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+
+
+/*imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';*/
