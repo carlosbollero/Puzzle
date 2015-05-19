001291868420b6ce3a4309bfac327d9690ce067e
Carlos <carlosmbollero@gmail.com>
2015-05-19 14:25:34 -0300

    Correciones para mejorar la lectura

--- puzzle.js
+++ puzzle.js
@@ -3,12 +3,10 @@
  *  http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg
  */
 
-
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var cw = canvas.width;
-var ch = canvas.height;
-    
+var ch = canvas.height;    
 
 var rows = 3;
 var cols = 3;
@@ -39,19 +37,20 @@ function start() {
         {col: 2, row: 2}];
     shuffle(pieces);
 
-        var i=0;
-        for(var y = 0;y < rows; y++){
+    var i=0;
+    for(var y = 0;y < rows; y++){
         for(var x = 0;x < cols; x++){
-        var p = pieces[i++];
-  ctx.drawImage(
-    // from the original image
-    img,
-    // take the next x,y piece
-    x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight,
-    // draw it on canvas based on the shuffled pieces[] array
-    p.col * pieceWidth, p.row * pieceHeight, pieceWidth, pieceHeight
-  );
-}}
+            var p = pieces[i++];
+            ctx.drawImage(
+            // from the original image
+            img,
+            // take the next x,y piece
+            x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight,
+            // draw it on canvas based on the shuffled pieces[] array
+            p.col * pieceWidth, p.row * pieceHeight, pieceWidth, pieceHeight
+            );
+        }
+    }
 
 
 }
